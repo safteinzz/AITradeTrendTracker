b0029c0ae5afd1998a257f17541b6122e07c25d0
Pablo Antonio Hernández <pablohernandezcontacto@gmail.com>
2021-11-15 08:46:10 +0100

    Commit prueba vscode ext

---
+++ main.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as pltcls
+import os
+import random
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout, Bidirectional
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from yahoo_fin import stock_info as si
+from collections import deque
+
+
